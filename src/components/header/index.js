1b828a391b3db67c55383a3fa27b9fcc98e533a0
Igor Chiriac <igorchiriac@outlook.com>
2018-03-05 23:06:49 +0100

    Add style for header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,20 @@
 import { h, Component } from "preact";
 import { Link } from "preact-router/match";
 import style from "./style";
+import avatar from "../../assets/avatar.jpg";
 
 export default class Header extends Component {
   render() {
     return (
       <header class={style.header}>
-        <div class="inner">
-          <a href="#" class="image avatar">
-            <img src="images/avatar.jpg" alt="" />
-          </a>
+        <div class={style.inner}>
+          <div class={style.avatar}>
+            <a href="#">
+              <img src={avatar} alt="Avatar picture" />
+            </a>
+          </div>
           <h1>
-            <strong>I am Igor Chiriac</strong>, a super<br />
+            I am <strong>Igor Chiriac</strong>, a super<br />
             frontend developer<br />
           </h1>
         </div>
